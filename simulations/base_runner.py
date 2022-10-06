cfa2ea46635f1c48f4db3568c4bb8f664630b788
ori cohen <ori@efrontier.io>
2022-10-06 22:22:17 +0300

    aurigmai error

--- simulations/base_runner.py
+++ simulations/base_runner.py
@@ -143,7 +143,7 @@ def create_account_information(SITE_ID, users_data, totalAssetCollateral, totalA
             data[asset]["top_10_debt"] = str(
                 users_data1.sort_values("DEBT_VST", ascending=False).head(10)["DEBT_VST"].sum())
 
-        if "nl_total_collateral" in users_data.columns:
+        if "nl_user_collateral" in users_data.columns:
             data[asset]["nl_total_collateral"] = str(users_data["NL_COLLATERAL_" + asset].sum())
             data[asset]["nl_median_collateral"] = str(
                 np.median(users_data.loc[users_data["COLLATERAL_" + asset] > 0]["NL_COLLATERAL_" + asset]))
