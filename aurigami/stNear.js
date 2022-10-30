c484bbe5878e9f2e623b110fe6d86246998e7cd9
yaron velner <yaron172@yahoo.com>
2022-10-30 11:24:52 +0200

    stnear urn

--- aurigami/stNear.js
+++ aurigami/stNear.js
@@ -9,15 +9,22 @@ async function checkMetapoolLiquidity(web3) {
     const nearAddress = "0xC42C30aC6Cc15faC9bD938618BcaA1a1FaE8501d"
     const near = new web3.eth.Contract(Addresses.erc20Abi, nearAddress)
     const nearDecimals = await near.methods.decimals().call()
+    
+    const stNEARAddress = "0x07F9F7f963C5cD2BBFFd30CcfB964Be114332E30"
+    const stNEAR = new web3.eth.Contract(Addresses.erc20Abi, stNEARAddress)
+    const stNEARDecimals = await stNEAR.methods.decimals().call()
 
     const metapoolAddress = "0x534BACf1126f60EA513F796a3377ff432BE62cf9"
     const nearBalance = await near.methods.balanceOf(metapoolAddress).call()
+    const stNEARBalance = await stNEAR.methods.balanceOf(metapoolAddress).call()
 
     const balance = toBN(nearBalance).div(toBN("10").pow(toBN(nearDecimals)))
+    const stbalance = toBN(stNEARBalance).div(toBN("10").pow(toBN(stNEARDecimals)))
 
     console.log(balance.toString())
+    console.log(stbalance.toString())    
 
-    fs.writeFileSync("stNEARLiquidity.json", JSON.stringify({"wNEARBalance" : balance.toString()}))
+    fs.writeFileSync("stNEARLiquidity.json", JSON.stringify({"wNEARBalance" : balance.toString(), "stNEARBalance" : stbalance.toString()}))
 }
 
 function sleep(ms) {
