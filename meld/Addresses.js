b2d26d2021e55894cb1eebec1388937a5d820524
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2023-01-09 17:43:52 +0100

    fix hex name from MELD dummy data

--- meld/Addresses.js
+++ meld/Addresses.js
@@ -29,21 +29,24 @@ const tokens = [
     },
     {
         symbol: 'WRT',
-        hexKey: '57696e67526964657273',
+        hexKey: '575254',
+        realhexKey: '57696e67526964657273',
         decimals: 6,
         address: 'c0ee29a85b13209423b10447d3c2e6a50641a15c57770e27cb9d507357696e67526964657273',
         poolId: '99d2ce140231b787c30c52ace4148951f2b54d66c9d7ebf29f89f5fbf8f7757b'
     },
     {
         symbol: 'COPI',
-        hexKey: '436f726e75636f70696173205b76696120436861696e506f72742e696f5d',
+        hexKey: '434f5049',
+        realhexKey: '436f726e75636f70696173205b76696120436861696e506f72742e696f5d',
         decimals: 6,
         address: 'b6a7467ea1deb012808ef4e87b5ff371e85f7142d7b356a40d9b42a0436f726e75636f70696173205b76696120436861696e506f72742e696f5d',
         poolId: '7925263b1aff069a191db67d5ac185c029f7f43e084a4ef6e5fa2848a56e2aa6'
     },
     {
         symbol: 'C3',
-        hexKey: '434841524c4933',
+        hexKey: '4333',
+        realhexKey: '434841524c4933',
         decimals: 6,
         address: '8e51398904a5d3fc129fbf4f1589701de23c7824d5c90fdb9490e15a434841524c4933',
         poolId: '2cb05a0cec80aa158dcc9faddf9de6a20a2acec78fa4101e3d271df7947fbcc5'
@@ -57,7 +60,8 @@ const tokens = [
     },
     {
         symbol: 'WMT',
-        hexKey: '776f726c646d6f62696c65746f6b656e',
+        hexKey: '574d54',
+        realhexKey: '776f726c646d6f62696c65746f6b656e',
         decimals: 6,
         address: '1d7f33bd23d85e1a25d87d86fac4f199c3197a2f7afeb662a0f34e1e776f726c646d6f62696c65746f6b656e',
         poolId: 'df88a770e300f248b776231b96da4525ce0f54bc457310efe27a03f2c14bb18f'
