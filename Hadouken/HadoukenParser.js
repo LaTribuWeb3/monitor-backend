a3ac0f9eedd639899c40faeb0f79aae0b14067b4
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-01-23 10:05:44 +0100

    Took into account sleeper in Aave.main

--- Hadouken/HadoukenParser.js
+++ Hadouken/HadoukenParser.js
@@ -5,7 +5,7 @@ const Addresses = require("./Addresses.js")
 async function HadoukenParser() {
     const web3 = new Web3("https://v1.mainnet.godwoken.io/rpc")
     const aave = new Aave(Addresses.hadoukenAddress, "GW", web3, "data.json")
-    await aave.main(false)
+    await aave.main(true)
  }
 
  module.exports = {HadoukenParser}
