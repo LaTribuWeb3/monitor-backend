bf7b3dfc686efc31597421f9c46b1ccc4a48404d
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2023-03-09 16:21:38 +0100

    fetch gas from RPC

--- simulations/kyber_prices.py
+++ simulations/kyber_prices.py
@@ -29,7 +29,43 @@ class KyberPrices:
         self.inv_names = inv_names
         self.chain_id = network
 
+    # get gas price from rpc fee history
+    def get_gas_price(self, chain_id):
+        print('getting gas price for chainid', chain_id)
+        rpcUrl = 'https://www.ankr.com/rpc/'
+        if chain_id == '100': # gnosis
+            rpcUrl = 'https://rpc.ankr.com/gnosis'
+        elif chain_id == '1313161554': # aurora
+            rpcUrl = 'https://mainnet.aurora.dev'
+        elif chain_id == '42161': # arbitrum
+            rpcUrl = 'https://rpc.ankr.com/arbitrum'
+
+        callDataJson = json.dumps({
+            "jsonrpc": "2.0",
+            "method": "eth_feeHistory",
+            "params": [
+                9,
+                "latest",
+                [25, 57]
+            ],
+            "id": 1
+        })
+
+        feeHistoryResponse = requests.post(rpcUrl, data=callDataJson)
+        feeHistoryResponseData = feeHistoryResponse.json()
+        fees = feeHistoryResponseData['result']['baseFeePerGas']
+
+        avgFee = 0
+        for fee in fees:
+            print('fee', fee,  int(fee, 16))
+            avgFee += int(fee, 16)
+        
+        avgFee = avgFee / len(fees)
+        return int(avgFee)
+    
     def get_price(self, base, quote, volume_in_base):
+        fnName = 'getPrice['+ base + '/' + quote + ']:'
+        print(fnName, 'start getting price for amount:', volume_in_base)
         token_in = self.underlying[self.inv_names[base]]
         token_out = self.underlying[self.inv_names[quote]]
         amount_in = volume_in_base * 10 ** self.decimals[self.inv_names[base]]
@@ -47,12 +83,11 @@ class KyberPrices:
 
         if private_config.use_one_inch_pathfinder: 
             now = datetime.datetime.now()
+            print(fnName, 'using 1inch pathfinder')
             if last_gas_price_fetch == None or (now - last_gas_price_fetch).total_seconds() > 120 : # fetch gas price every 2 minutes
-                etherscanGastResponse = requests.get('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken')
-                etherscanGastResponseData = etherscanGastResponse.json()
+                current_gas_price = self.get_gas_price(self.chain_id)
                 last_gas_price_fetch = datetime.datetime.now()
-                current_gas_price = int(int(etherscanGastResponseData['result']['ProposeGasPrice']) * 1e9)
-                print('updated gas price to', current_gas_price)
+                print(fnName, 'updated gas price to', current_gas_price)
 
             url_to_send = 'https://pathfinder.1inch.io/v1.4/chain/'+str(self.chain_id)+'/router/v5/quotes?fromTokenAddress='+ str(token_in) + \
                             '&toTokenAddress='+str(token_out)+'&amount='+str(int(amount_in))+'&preset=maxReturnResult&gasPrice='+ str(current_gas_price)
@@ -77,7 +112,11 @@ class KyberPrices:
                 else:
                     response_amount_out = int(data["toTokenAmount"]) / 10 ** self.decimals[self.inv_names[quote]]
 
+                print(fnName, 'url used', url_to_send)
+                print(fnName, 'response amount', response_amount_out)
+
                 price_in_base = response_amount_in / response_amount_out
+                print(fnName, 'price_in_base', price_in_base)
                 return price_in_base
             except Exception as e:
                 print(e)
