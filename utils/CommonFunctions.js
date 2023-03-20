81a9b35e35031943f2e84a22d23a192adce9bd6b
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-03-20 10:31:56 +0100

    Hadouken parser now retries on failed RPC call

---
+++ utils/CommonFunctions.js
@@ -0,0 +1,29 @@
+/**
+ * a small retry wrapper with an incrameting 5s sleep delay
+ * @param {*} fn 
+ * @param {*} params 
+ * @param {*} retries 
+ * @param {*} maxRetries
+ * @returns 
+ */
+async function retry(fn, params, retries = 0, maxRetries = 50) {
+    try {
+        const res = await  fn(...params);
+        if(retries){
+            console.log(`retry success after ${retries} retries`);
+        } else {
+            console.log('success on first try');
+        }
+        return res;
+    } catch (e) {
+        console.error(e);
+        retries++;
+        if(retries >= maxRetries){
+            throw(e);
+        } 
+        console.log(`retry #${retries}`);
+        await new Promise(resolve => setTimeout(resolve, 1000 * 5 * retries));
+        return retry(fn, params, retries);
+    }
+}
+module.exports = { retry };
