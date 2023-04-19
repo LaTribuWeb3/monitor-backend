b6be65da47868b860e0394581441c5c438400fe4
Alfulinku <Alfulinku@gmail.com>
2023-04-19 15:00:42 +0200

    sum USDC and hUSDC and USDT and hUSDT

--- Hadouken/HadoukenSpecificLiquidity.js
+++ Hadouken/HadoukenSpecificLiquidity.js
@@ -5,10 +5,10 @@ const {hadoukenVaultAbi, linearPoolABI, hadoukenVaultAddress, hadoukenUSDPoolABI
 const { retry } = require("../utils/CommonFunctions.js");
 const { BigNumber } = require("ethers");
 const USDCAddress = '0x186181e225dc1Ad85a4A94164232bD261e351C33';
-const boostedUSDCAddress = "0x149916D7128C36bbcebD04F794217Baf51085fB9"
+const hUSDCAddress = '0xE16AE54fd2b74D92F9FeD49Bf7fA20aaB003DD60';
 
 const USDTAddress = '0x8E019acb11C7d17c26D334901fA2ac41C1f44d50';
-const boostedUSDTAddress = "0xa0430F122fb7E4F6F509c9cb664912C2f01db3e2"
+const hUSDTAddress = '0xb434d6E7945C755c6B16DF386A4b0BE2bfd2d4a5';
 
 const BoostedUSDPoolId = '0xaf9d4028272f750dd2d028990fd664dc223479b1000000000000000000000013';
 async function hadoukenUSDLiquidityFetcher() {
@@ -21,35 +21,42 @@ async function hadoukenUSDLiquidityFetcher() {
         const ampFactor = Number(ampParameters["value"]) / Number(ampParameters["precision"]);
         const vault = new web3.eth.Contract(hadoukenVaultAbi, hadoukenVaultAddress);
 
-        const liquidity = await retry(vault.methods.getPoolTokens(BoostedUSDPoolId).call, []);
+        const linearUSDCPoolAddress = '0x149916d7128c36bbcebd04f794217baf51085fb9';
+        const USDCPoolContract = new web3.eth.Contract(linearPoolABI, linearUSDCPoolAddress);
+        const USDCPoolId = await retry(USDCPoolContract.methods.getPoolId().call, []);
+        const LinearUSDCliquidity = await retry(vault.methods.getPoolTokens(USDCPoolId).call, []);
+        console.log(LinearUSDCliquidity)
+        const indexOfUSDC = LinearUSDCliquidity.tokens.indexOf(USDCAddress);
+        const balanceOfUSDC = LinearUSDCliquidity.balances[indexOfUSDC];
+        const indexOfhUSDC = LinearUSDCliquidity.tokens.indexOf(hUSDCAddress);
+        const balanceOfhUSDC = LinearUSDCliquidity.balances[indexOfhUSDC];
+        const sumBalanceUSDC = BigNumber.from(balanceOfUSDC).add(BigNumber.from(balanceOfhUSDC));
+        console.log('balanceOfUSDC', sumBalanceUSDC.toString());
 
-        // we will consider boostedUSDC and boostedUSDT to be USDC and USDT
-        const indexOfBoostedUSDC = liquidity.tokens.indexOf(boostedUSDCAddress);
-        const indexOfBoostedUSDT = liquidity.tokens.indexOf(boostedUSDTAddress);
-
-        const balanceOfBoostedUSDC = liquidity.balances[indexOfBoostedUSDC];
-        const balanceOfBoostedUSDT = liquidity.balances[indexOfBoostedUSDT];
-        
-        // boosted liquidity have 18 decimals, need to short it to 6 like USDC and USDT,
-        // so we divide by 1e12 (18 - 6)
-        const _1e12 = BigNumber.from(10).pow(12);
-        const usdcBalance = BigNumber.from(balanceOfBoostedUSDC).div(_1e12).toString()
-        console.log(usdcBalance)
-        const usdtBalance = BigNumber.from(balanceOfBoostedUSDT).div(_1e12).toString()
-        console.log(usdtBalance)
+        const linearUSDTPoolAddress = '0xa0430f122fb7e4f6f509c9cb664912c2f01db3e2';
+        const USDTPoolContract = new web3.eth.Contract(linearPoolABI, linearUSDTPoolAddress);
+        const USDTPoolId = await retry(USDTPoolContract.methods.getPoolId().call, []);
+        const LinearUSDTliquidity = await retry(vault.methods.getPoolTokens(USDTPoolId).call, []);
+        console.log(LinearUSDTliquidity)
+        const indexOfUSDT = LinearUSDTliquidity.tokens.indexOf(USDTAddress);
+        const balanceOfUSDT = LinearUSDTliquidity.balances[indexOfUSDT];
+        const indexOfhUSDT = LinearUSDTliquidity.tokens.indexOf(hUSDTAddress);
+        const balanceOfhUSDT = LinearUSDTliquidity.balances[indexOfhUSDT];
+        const sumBalanceUSDT = BigNumber.from(balanceOfUSDT).add(BigNumber.from(balanceOfhUSDT));
+        console.log('balanceOfUSDT', sumBalanceUSDT.toString());
 
         formattedOutput = {}
         const UsdcUsdt = `${USDCAddress}_${USDTAddress}`;
         const UsdtUsdc = `${USDTAddress}_${USDCAddress}`;
         formattedOutput['lastUpdate'] = Math.floor(Date.now() / 1000);
         formattedOutput[UsdcUsdt] = {
-            token0: usdcBalance,
-            token1: usdtBalance,
+            token0: sumBalanceUSDC.toString(),
+            token1: sumBalanceUSDT.toString(),
             ampFactor: ampFactor
         };
         formattedOutput[UsdtUsdc] = {
-            token0: usdtBalance,
-            token1: usdcBalance,
+            token0: sumBalanceUSDT.toString(),
+            token1: sumBalanceUSDC.toString(),
             ampFactor: ampFactor
         };
 
