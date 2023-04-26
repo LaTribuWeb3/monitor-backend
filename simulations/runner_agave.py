6360724f93bdfcb9eea437bd7fc91309192d9658
risk-dao-pusher <ori@efrontier.io>
2023-04-26 12:43:41 +0300

    Update runner_agave.py

--- simulations/runner_agave.py
+++ simulations/runner_agave.py
@@ -62,11 +62,11 @@ def create_simulation_config(SITE_ID, c, ETH_PRICE, assets_to_simulate, assets_a
                 new_c["series_std_ratio"] = std_ratio
                 new_c["volume_for_slippage_10_percentss"] = [slippage]
                 new_c["json_time"] = now_time
-                if "DAI" in base_to_simulation or "DAI" in quote_to_simulation:
-                    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx")
-                    new_c["recovery_halflife_retails"] = [2]
-                else:
-                    new_c["recovery_halflife_retails"] = [0]
+                # if "DAI" in base_to_simulation or "DAI" in quote_to_simulation:
+                #     print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx")
+                #     new_c["recovery_halflife_retails"] = [2]
+                # else:
+                #     new_c["recovery_halflife_retails"] = [0]
 
                 max_collateral = collateral_caps[inv_names[base_to_simulation]]
                 max_debt = borrow_caps[inv_names[quote_to_simulation]]
